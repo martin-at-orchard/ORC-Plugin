ab9b6cd07cc1fe76561fd4d98bbd1b75265f7582
Martin Wedepohl <martin.wedepohl@shaw.ca>
2020-04-27 21:18:24 -0700

    0.3.2 Allow click for admissions and testimonials

--- readme.md
+++ readme.md
@@ -71,6 +71,9 @@ If you get stuck, or have questions you can email Martin Wedepohl martin@wedepoh
 
 ## Changelog
 
+### 2020-04-27 Version: 0.3.2
+* Allow click for admissions and testimonials
+
 ### 2020-04-27 Version: 0.3.1
 * Testimonials are now in a blockquote
 * Fixed version for the package.json
--- src/js/frontend.js
+++ src/js/frontend.js
@@ -1,13 +1,20 @@
 'use strict';
 
 /**
- * Check to see if the program or the staff objects are clicked or
+ * Check to see if the program, staff, admission or testimonial objects are clicked or
  * if a child of the object is clicked.
  */
 document.addEventListener('click', function (event) {
 
-	let obj_match    = event.target.matches( '.program' ) || event.target.matches( '.staff' );
-	let parent_match = event.target.parentElement.matches( '.program' ) || event.target.parentElement.matches( '.staff' );
+	let obj_match    = event.target.matches( '.program' ) ||
+						event.target.matches( '.staff' ) ||
+						event.target.matches( '.admission' ) ||
+						event.target.matches( '.testimonial' );
+
+	let parent_match = event.target.parentElement.matches( '.program' ) || 
+						event.target.parentElement.matches( '.staff' ) ||
+						event.target.parentElement.matches( '.admission' ) ||
+						event.target.parentElement.matches( '.testimonial' );
 
 	if ( obj_match || parent_match ) {
 		let span = '';
